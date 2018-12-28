4a0db870be244e887f32c7525d3b0d1d5e3e3056
xDotDash <xdotdash@gmail.com>
2018-12-27 22:11:44 -0600

    EKeys#SELECTED_ABILITY -> EKeys#SELECTED_ABILITIES

--- src/main/kotlin/elements/api/data/EKeys.kt
+++ src/main/kotlin/elements/api/data/EKeys.kt
@@ -1,12 +1,12 @@
 package elements.api.data
 
 import org.spongepowered.api.data.key.Key
-import org.spongepowered.api.data.value.BaseValue
+import org.spongepowered.api.data.value.mutable.ListValue
 import org.spongepowered.api.util.generator.dummy.DummyObjectProvider
 
 object EKeys {
 
     @JvmField
-    val SELECTED_ABILITY: Key<out BaseValue<String>> =
+    val SELECTED_ABILITIES: Key<ListValue<String>> =
         DummyObjectProvider.createExtendedFor(Key::class.java, "SELECTED_ABILITY")
 }
