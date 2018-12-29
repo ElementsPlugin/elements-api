ed0a0393af165171834a3d77a559a91697c252af
xDotDash <xdotdash@gmail.com>
2018-12-28 22:05:30 -0600

    Most likely version 1.0.0

--- src/main/kotlin/elements/api/data/ability/AbilityData.kt
+++ src/main/kotlin/elements/api/data/ability/AbilityData.kt
@@ -1,9 +1,10 @@
 package elements.api.data.ability
 
+import elements.api.ability.Ability
 import org.spongepowered.api.data.manipulator.mutable.ListData
 import org.spongepowered.api.data.value.mutable.ListValue
 
-interface AbilityData : ListData<String, AbilityData, ImmutableAbilityData> {
+interface AbilityData : ListData<Ability, AbilityData, ImmutableAbilityData> {
 
-    val selected: ListValue<String>
+    val selected: ListValue<Ability>
 }
--- src/main/kotlin/elements/api/data/ability/ImmutableAbilityData.kt
+++ src/main/kotlin/elements/api/data/ability/ImmutableAbilityData.kt
@@ -1,9 +1,10 @@
 package elements.api.data.ability
 
+import elements.api.ability.Ability
 import org.spongepowered.api.data.manipulator.immutable.ImmutableListData
 import org.spongepowered.api.data.value.immutable.ImmutableListValue
 
-interface ImmutableAbilityData : ImmutableListData<String, ImmutableAbilityData, AbilityData> {
+interface ImmutableAbilityData : ImmutableListData<Ability, ImmutableAbilityData, AbilityData> {
 
-    val selected: ImmutableListValue<String>
+    val selected: ImmutableListValue<Ability>
 }
