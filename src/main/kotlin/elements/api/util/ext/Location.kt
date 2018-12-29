80a4e2b3f62eeabc109c03bda630c6b11f6bd282
xDotDash <xdotdash@gmail.com>
2018-12-28 18:09:54 -0600

    Add Location<World>#spawnParticles

--- src/main/kotlin/elements/api/util/ext/Location.kt
+++ src/main/kotlin/elements/api/util/ext/Location.kt
@@ -2,6 +2,7 @@ package elements.api.util.ext
 
 import com.flowpowered.math.vector.Vector3d
 import com.flowpowered.math.vector.Vector3i
+import org.spongepowered.api.effect.particle.ParticleEffect
 import org.spongepowered.api.entity.Entity
 import org.spongepowered.api.world.Location
 import org.spongepowered.api.world.World
@@ -20,3 +21,9 @@ operator fun Location<World>.minus(v: Vector3d): Location<World> =
 
 operator fun Location<World>.minus(v: Vector3i): Location<World> =
     this.sub(v)
+
+fun Location<World>.spawnParticles(particleEffect: ParticleEffect) =
+    extent.spawnParticles(particleEffect, position)
+
+fun Location<World>.spawnParticles(particleEffect: ParticleEffect, radius: Int) =
+    extent.spawnParticles(particleEffect, position, radius)
