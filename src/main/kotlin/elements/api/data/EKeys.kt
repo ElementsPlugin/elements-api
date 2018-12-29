51440e8f1c3792db0cc3ed64fd6cb83f16f7a15e
xDotDash <xdotdash@gmail.com>
2018-12-28 22:07:57 -0600

    Missed a spot

--- src/main/kotlin/elements/api/data/EKeys.kt
+++ src/main/kotlin/elements/api/data/EKeys.kt
@@ -1,5 +1,6 @@
 package elements.api.data
 
+import elements.api.ability.Ability
 import org.spongepowered.api.data.key.Key
 import org.spongepowered.api.data.value.mutable.ListValue
 import org.spongepowered.api.util.generator.dummy.DummyObjectProvider
@@ -7,6 +8,6 @@ import org.spongepowered.api.util.generator.dummy.DummyObjectProvider
 object EKeys {
 
     @JvmField
-    var SELECTED_ABILITIES: Key<ListValue<String>> =
+    var SELECTED_ABILITIES: Key<ListValue<Ability>> =
         DummyObjectProvider.createExtendedFor(Key::class.java, "SELECTED_ABILITY")
 }
