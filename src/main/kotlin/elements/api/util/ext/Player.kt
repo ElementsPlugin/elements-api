fc959ba4b1b050138f19ddf92407159d3f1f5a3e
xDotDash <xdotdash@gmail.com>
2018-12-29 23:26:33 -0600

    Add Player#selectedSlotIndex extension method

--- src/main/kotlin/elements/api/util/ext/Player.kt
+++ src/main/kotlin/elements/api/util/ext/Player.kt
@@ -4,9 +4,16 @@ import com.flowpowered.math.imaginary.Quaterniond
 import com.flowpowered.math.vector.Vector3d
 import org.spongepowered.api.data.property.entity.EyeLocationProperty
 import org.spongepowered.api.entity.living.player.Player
+import org.spongepowered.api.item.inventory.entity.PlayerInventory
 
 val Player.direction: Vector3d
     get() = Quaterniond.fromAxesAnglesDeg(rotation.x, -rotation.y, -rotation.z).direction
 
 val Player.eyePosition: Vector3d
     get() = getProperty(EyeLocationProperty::class.java).unwrap()?.value ?: position
+
+var Player.selectedSlotIndex: Int
+    get() = (inventory as PlayerInventory).hotbar.selectedSlotIndex
+    set(value) {
+        (inventory as PlayerInventory).hotbar.selectedSlotIndex = value
+    }
