b40e3d34a979b79531fc24f2e64c49e7fa2ac4d0
xDotDash <xdotdash@gmail.com>
2018-12-30 11:50:45 -0600

    oops

--- src/main/kotlin/elements/api/ability/AbstractAbility.kt
+++ src/main/kotlin/elements/api/ability/AbstractAbility.kt
@@ -2,6 +2,7 @@ package elements.api.ability
 
 import elements.api.element.Element
 import elements.api.ability.result.AbilityResult
+import elements.api.activator.Activator
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.text.Text
 import sporoutines.CoroutineTaskManager
@@ -14,5 +15,5 @@ abstract class AbstractAbility(private val id: String, private val name: String,
 
     override fun getName(): String = name
 
-    abstract override suspend fun CoroutineTaskManager.execute(player: Player): AbilityResult
+    abstract override suspend fun CoroutineTaskManager.execute(player: Player, activator: Activator): AbilityResult
 }
